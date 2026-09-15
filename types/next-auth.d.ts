929c39f9e5b1644a06b3b10930e5a0b185e02852
Richard <richardlapuente@hotmail.com>
2026-09-15 14:25:58 -0300

    Excluindo prisma.config e forçando tipos

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,7 +1,6 @@
 import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  // Estendemos a sessão padrão para incluir o 'id'
   interface Session {
     user: {
       id: string;
